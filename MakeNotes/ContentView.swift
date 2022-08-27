b997dcf08d311f9d483dd96bd36cc08739dd5024
Nowroz <isl.nowroz@gmail.com>
2022-08-27 19:07:41 +0600

    Initial Commit

---
+++ MakeNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MakeNotes
+//
+//  Created by Nowroz Islam on 27/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakeNotes/MakeNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeNotesApp.swift
+//  MakeNotes
+//
+//  Created by Nowroz Islam on 27/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MakeNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
