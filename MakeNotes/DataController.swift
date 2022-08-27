3188fe421b29659c4e19017f4e232095e241fa27
Nowroz <isl.nowroz@gmail.com>
2022-08-28 00:26:32 +0600

    Pass ManagedObjectContext into Environment
    A DataController class is created for the MakeNotes Data Model and its
    instance, dataController is initiated inside MakeNotesApp struct. The
    viewContext of the dataController instance is then passed to the
    ContentView's environment as the managedObjectContext.

--- MakeNotes/DataController.swift
+++ MakeNotes/DataController.swift
@@ -5,4 +5,17 @@
 //  Created by Nowroz Islam on 27/8/22.
 //
 
+import CoreData
 import Foundation
+
+@MainActor class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "MakeNotes")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
