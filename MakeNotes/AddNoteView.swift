f8052dd55286516744d3a2d54126fbcc571c707b
Nowroz <isl.nowroz@gmail.com>
2022-08-28 14:18:24 +0600

    Create AddNoteView File

---
+++ MakeNotes/AddNoteView.swift
@@ -0,0 +1,20 @@
+//
+//  AddNoteView.swift
+//  MakeNotes
+//
+//  Created by Nowroz Islam on 28/8/22.
+//
+
+import SwiftUI
+
+struct AddNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddNoteView()
+    }
+}
