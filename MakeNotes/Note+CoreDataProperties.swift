bba8ff8754d70ee4e828162e80854a3523827798
Nowroz <isl.nowroz@gmail.com>
2022-08-28 20:26:48 +0600

    Handle Optionals in Note Class
    Three computed variables are added to handle the three attributes in
    Note entity's NSManagedObject Subclass.

--- MakeNotes/Note+CoreDataProperties.swift
+++ MakeNotes/Note+CoreDataProperties.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreData
+import SwiftUI
 
 
 extension Note {
@@ -19,6 +20,18 @@ extension Note {
     @NSManaged public var body: String?
     @NSManaged public var title: String?
     @NSManaged public var lastEdited: Date?
+    
+    public var noteTitle: String {
+        title ?? "Unknown Title"
+    }
+    
+    public var noteBody: LocalizedStringKey {
+        LocalizedStringKey(body ?? "Unknown...")
+    }
+    
+    public var lastEditedDate: Date {
+        lastEdited ?? Date.now
+    }
 
 }
 
