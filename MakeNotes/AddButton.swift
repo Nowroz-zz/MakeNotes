9ba6d2a6904185c2584448eafeb197655d2ad3b8
Nowroz <isl.nowroz@gmail.com>
2022-08-28 13:27:52 +0600

    Create AddButton File

---
+++ MakeNotes/AddButton.swift
@@ -0,0 +1,20 @@
+//
+//  AddButton.swift
+//  MakeNotes
+//
+//  Created by Nowroz Islam on 28/8/22.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AddButton()
+    }
+}
